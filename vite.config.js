314271faaaa9a7bc7c3c9ed3484a35a548a565d1
obetancourthunicah <obetancourthunicah@gmail.com>
2023-10-25 19:36:29 -0600

    Base Url Error showcase

--- vite.config.js
+++ vite.config.js
@@ -21,5 +21,5 @@ export default defineConfig({
         htmlPurge({}),
         ViteMinifyPlugin({}),
     ],
-    base: "/github_pages_actions_deploy/"
+    base: "/github_pages_actions_deploy_1/"
 })
